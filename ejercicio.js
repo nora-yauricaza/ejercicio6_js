3888dfbaa7a2eef537cef427756ee3afc21ec9a9
nora-yauricaza <nora-yauricaza>
2016-08-25 17:46:16 -0500

    ejercicio6

---
+++ ejercicio.js
@@ -0,0 +1,12 @@
+var numero = prompt("Ingresa un número entero");
+var resultado = parImpar(numero);
+alert("El número "+numero+" es "+resultado);
+ 
+function parImpar(numero) {
+  if(numero % 2 == 0) {
+    return "par";
+  }
+  else {
+    return "impar";
+  }
+}
